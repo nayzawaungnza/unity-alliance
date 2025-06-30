86dee276bbb0d0d0d6440758bc661229e228486c
nayzawaungnza <nayzawaung750@gmail.com>
2025-06-30 15:45:41 +0630

    vite up

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   base: '/', // Ensure base path is correct for Vercel
+  assetsInclude: ['**/*.jpg', '**/*.png'],
   plugins: [react()],
 })
